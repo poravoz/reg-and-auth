2ffbf892a785179ee005e15d0bea60e98b247bd4
poravoz <116353344+poravoz@users.noreply.github.com>
2023-09-24 00:36:40 +0300

    24.09.2023

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,21 +1,29 @@
-import { Body, Req, Controller, HttpCode, Post, UseGuards, Res } from '@nestjs/common';
+import { Body, Req, Controller, HttpCode, Post, UseGuards, Res, Get } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import RegisterDto from './dto/register.dto';
 import RequestWithUser from './interfaces/requestWithUser.interface';
 import { LocalAuthenticationGuard } from './jwt-strategy/localAuthentication.guard';
 import { Response } from 'express';
+import JwtAuthenticationGuard from './jwt-strategy/jwt-authentication.guard';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(
     private readonly authenticationService: AuthenticationService
   ) {}
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Get()
+  authenticate(@Req() request: RequestWithUser) {
+    const user = request.user;
+    user.password = undefined;
+    return user;
+  }
  
   @Post('register')
   async register(@Body() registrationData: RegisterDto) {
     return this.authenticationService.register(registrationData);
   }
- 
 
   @HttpCode(200)
   @UseGuards(LocalAuthenticationGuard)
@@ -27,4 +35,12 @@ export class AuthenticationController {
     user.password = undefined;
     return response.send(user);
   }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Post('log-out')
+  async logOut(@Req() request: RequestWithUser, @Res() response: Response) {
+    response.setHeader('Set-Cookie', this.authenticationService.getCookieForLogOut());
+    return response.sendStatus(200);
+  }
+
 }
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -56,4 +56,8 @@ export class AuthenticationService {
         const token = this.jwtService.sign(payload);
         return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`;
       }
+
+      public getCookieForLogOut() {
+        return `Authentication=; HttpOnly; Path=/; Max-Age=0`;
+      }
   }
