ea8504276438dd740583416f20f1f6be687c48b3
poravoz <116353344+poravoz@users.noreply.github.com>
2023-09-24 00:14:06 +0300

    24.09.2023

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -4,9 +4,25 @@ import { UsersModule } from '../users/users.module';
 import { AuthenticationController } from './authentication.controller';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './jwt-strategy/local.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
  
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule, 
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+          },
+        }),
+      }),
+    ],
   providers: [AuthenticationService, LocalStrategy],
   controllers: [AuthenticationController]
 })
