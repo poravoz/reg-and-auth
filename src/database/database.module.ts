489d4139a29b5429091bf4507726ba6a7a6a72a0
poravoz <116353344+poravoz@users.noreply.github.com>
2023-09-23 23:42:41 +0300

    23.09.2023

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import User from 'src/users/user.entity';
+import User from 'src/users/entities/user.entity';
  
 @Module({
   imports: [
