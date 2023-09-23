ab4f9be77e5c5c2e900aacd34c25bfc29a400429
poravoz <116353344+poravoz@users.noreply.github.com>
2023-09-24 00:19:46 +0300

    24.09.2023

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Req, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { Body, Req, Controller, HttpCode, Post, UseGuards, Res } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import RegisterDto from './dto/register.dto';
 import RequestWithUser from './interfaces/requestWithUser.interface';
 import { LocalAuthenticationGuard } from './jwt-strategy/localAuthentication.guard';
- 
+import { Response } from 'express';
+
 @Controller('authentication')
 export class AuthenticationController {
   constructor(
@@ -15,12 +16,15 @@ export class AuthenticationController {
     return this.authenticationService.register(registrationData);
   }
  
+
   @HttpCode(200)
   @UseGuards(LocalAuthenticationGuard)
   @Post('log-in')
-  async logIn(@Req() request: RequestWithUser) {
-    const user = request.user;
+  async logIn(@Req() request: RequestWithUser, @Res() response: Response) {
+    const {user} = request;
+    const cookie = this.authenticationService.getCookieWithJwtToken(user.id);
+    response.setHeader('Set-Cookie', cookie);
     user.password = undefined;
-    return user;
+    return response.send(user);
   }
 }
