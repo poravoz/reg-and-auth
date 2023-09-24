c4e0c745ca5d956b61f9e6c272edee9786e4caac
poravoz <116353344+poravoz@users.noreply.github.com>
2023-09-24 15:13:04 +0300

    24.09.2023

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,9 @@ import { Controller, Post, Body, Get, Param, Delete} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/createUser.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @Get()
   getAll() {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import User from './entities/user.entity';
+import { UserController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
+  controllers: [UserController],
   exports: [UsersService]
 })
 export class UsersModule {}
