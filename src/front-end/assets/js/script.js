a150a08aef26148513ddc632131d76e5fabab942
poravoz <116353344+poravoz@users.noreply.github.com>
2023-09-25 11:03:50 +0300

    25.09.2023

--- src/front-end/assets/js/script.js
+++ src/front-end/assets/js/script.js
@@ -1,7 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
     const form = document.querySelector('form');
   
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', async function(event) {
+      event.preventDefault(); // Предотвращаем стандартное поведение формы (перезагрузка страницы)
+  
       const name = document.getElementById('name').value;
       const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
@@ -9,34 +11,56 @@ document.addEventListener('DOMContentLoaded', function() {
   
       if (!name || !email || !password || !repeatPassword) {
         alert('Please fill in all fields');
-        event.preventDefault();
+        return;
+      } else if (password.length < 8) {
+        alert('Password should be at least 8 characters long');
+        return;
       } else if (password !== repeatPassword) {
         alert('Password mismatch');
-        event.preventDefault(); 
-      } else {
-        event.preventDefault(); 
+        return;
+      }
   
-        const data = {
-          name: name,
-          email: email,
-          password: password
-        };
+      // Проверяем, существует ли пользователь с таким email
+      const response = await fetch('http://localhost:3000/authentication/check-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: email })
+      });
   
-        fetch('http://localhost:3000/authentication/register', {
+      const data = await response.json();
+      if (data.emailExists) {
+        alert('User with this email already exists');
+        return;
+      }
+  
+      // Если пользователь с таким email не существует, отправляем запрос на регистрацию
+      const dataToServer = {
+        name: name,
+        email: email,
+        password: password
+      };
+  
+      try {
+        const response = await fetch('http://localhost:3000/authentication/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(data)
-        })
-          .then(response => response.json())
-          .then(data => {
-            console.log('Successfully registered:', data);
-            window.location.href = './authentication.html';
-          })
-          .catch(error => {
-            console.error('Registration error:', error);
-          });
+          body: JSON.stringify(dataToServer)
+        });
+  
+        const responseData = await response.json();
+        console.log('Successfully registered:', responseData);
+  
+        if (responseData.id) {
+          window.location.href = './authentication.html';
+        } else {
+          alert('Registration failed');
+        }
+      } catch (error) {
+        console.error('Registration error:', error);
       }
     });
-  });
+  };
