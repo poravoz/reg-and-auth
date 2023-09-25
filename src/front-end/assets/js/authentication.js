bc4c13ea228ed717b9b3295c1da159dce24b4676
poravoz <116353344+poravoz@users.noreply.github.com>
2023-09-25 11:25:13 +0300

    25.09.2023

---
+++ src/front-end/assets/js/authentication.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+    const form = document.querySelector('form');
+  
+    form.addEventListener('submit', async function(event) {
+      event.preventDefault(); 
+  
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      if (!email || !password) {
+        alert('Please fill in all fields');
+        return;
+      }
+  
+      const data = {
+        email: email,
+        password: password
+      };
+  
+      try {
+        const response = await fetch('http://localhost:3000/authentication/log-in', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+  
+        const responseData = await response.json();
+  
+        if (response.ok) {
+          console.log('Successfully authenticated:', responseData);
+          window.location.href = '../index.html';
+        } else {
+          alert('Authentication failed');
+        }
+      } catch (error) {
+        console.error('Authentication error:', error);
+      }
+    });
+  };
